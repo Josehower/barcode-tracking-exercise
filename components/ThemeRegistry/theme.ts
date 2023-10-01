08921eb1df64dc2f88c0f60e9a1b89b532776851
José Fernando Höwer Barbosa <josehowerupn@gmail.com>
2023-10-01 10:26:21 +0200

    Initialize project

---
+++ components/ThemeRegistry/theme.ts
@@ -0,0 +1,42 @@
+import { createTheme } from '@mui/material/styles';
+import { Roboto } from 'next/font/google';
+
+const roboto = Roboto({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+const theme = createTheme({
+  palette: {
+    background: {
+      default: '#FAF9F6',
+    },
+    text: {
+      primary: '#0e1318',
+      secondary: '#FAF9F6',
+    },
+    primary: {
+      main: '#FFCD87',
+    },
+    secondary: {
+      main: '#87B9FF',
+    },
+  },
+  typography: {
+    fontFamily: roboto.style.fontFamily,
+  },
+  components: {
+    MuiAlert: {
+      styleOverrides: {
+        root: ({ ownerState }) => ({
+          ...(ownerState.severity === 'info' && {
+            backgroundColor: '#60a5fa',
+          }),
+        }),
+      },
+    },
+  },
+});
+
+export default theme;
