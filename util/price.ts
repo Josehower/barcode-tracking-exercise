975390c6ef587ea900c917d0b82ca430dad2a4c5
José Fernando Höwer Barbosa <josehowerupn@gmail.com>
2023-10-04 22:07:03 +0200

    Improve calculate Price redeability

--- util/price.ts
+++ util/price.ts
@@ -1,14 +1,23 @@
 import { TicketWithBillings } from '../database/tickets';
 import { msToFullHours } from './time';
 
-export function calculatePrice(ticket: TicketWithBillings, serverTime: string) {
+export function calculatePrice(
+  ticket: TicketWithBillings,
+  endingTimestamp: string,
+) {
+  const { baseHours, basePrice, extraHourPrice } = {
+    baseHours: 3,
+    basePrice: 30,
+    extraHourPrice: 3,
+  };
+
   const startingTimeMs = +new Date(ticket.checkinTimestamp);
-  const serverTimeMs = +new Date(serverTime);
+  const endingTimeMs = +new Date(endingTimestamp);
 
-  const serviceHours = msToFullHours(serverTimeMs - startingTimeMs);
+  const serviceHours = msToFullHours(endingTimeMs - startingTimeMs);
+  const extraHours = serviceHours > baseHours ? serviceHours - baseHours : 0;
 
-  const preliminaryPrice = (serviceHours - 3) * 3 + 30;
-  const totalPrice = preliminaryPrice < 30 ? 30 : preliminaryPrice;
+  const totalPrice = basePrice + extraHours * extraHourPrice;
 
   return ticket.billingHistory.reduce((previous, current) => {
     return previous - current.amount;
